a053c1f2a16d461506b223f5613c24838cee9e4e
Glauco Munsberg <glaucomunsberg@gmail.com>
2019-11-03 12:18:26 -0300

    fixing js numbers animate

--- js/index.js
+++ js/index.js
@@ -19,16 +19,16 @@ setTimeout(() => {
     $("#iframe").prop("hidden", false);
 }, 2500)
 
-let bool = true;
-$(document).scroll(() => {
-    if ((window.scrollY > $(".numeros").offset().top - window.innerHeight / 1.25) && bool) {
-        $('.counter1').animateNumber({ number: 455 }, 1200);
-        $('.counter2').animateNumber({ number: 190 }, 800);
-        $('.counter3').animateNumber({ number: 9100 }, 800);
-        $('.counter4').animateNumber({ number: 3599 }, 1000);
-        bool = false;
-    }
-});
+//let bool = true;
+//$(document).scroll(() => {
+//    if ((window.scrollY > $(".numeros").offset().top - window.innerHeight / 1.25) && bool) {
+//        $('.counter1').animateNumber({ number: 455 }, 1200);
+//        $('.counter2').animateNumber({ number: 190 }, 800);
+//        $('.counter3').animateNumber({ number: 9100 }, 800);
+//        $('.counter4').animateNumber({ number: 3599 }, 1000);
+//        bool = false;
+//    }
+//});
 
 $('.klaes').on('click', () =>{
     window.open("https://www.linkedin.com/in/klaesrodrigo/");
